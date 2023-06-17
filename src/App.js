acd15336187a81088f8f8152b941dfafd89a777d
syahrul <syahrulazis0706@.com>
2023-06-17 16:03:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Routes, NavLink, HashRouter, } from "react-router-dom";
+import './style.css';
+import Home from "./Home";
+import About from "./About";
+import Contact from "./Contact";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Main extends Component {
+  render() {
+    return (
+      <HashRouter>
+
+        <div>
+          <h1 className="title">Single Page Application </h1>
+          <ul className="header">
+            <li> <NavLink to="/">Home</NavLink></li>
+            <li> <NavLink to="/about">About</NavLink></li>
+            <li> <NavLink to="/contact">Contact</NavLink></li>
+          </ul>
+          <div className="content">
+            <Routes exact path="/" component={Home}/>
+            <Routes exact path="/about" component={About}/>
+            <Routes exact path="/contact" component={Contact}/>
+          </div>
+
+        </div>
+      </HashRouter>
+    );
+  }
+
 }
 
-export default App;
+export default Main;
